3c5efd6f58d427310cd75a0ebd91a1dab17c4edc
jagadeesh <jagadeeshmaroju2056@gmail.com>
2018-05-09 02:31:08 +0530

    Docs build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DocsComponent } from './docs/docs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocsComponent
   ],
   imports: [
     BrowserModule
